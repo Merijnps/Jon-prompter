2f448d767e9871901a6ea7a1848fc0b0563c713b
Merijnps <144733569+Merijnps@users.noreply.github.com>
2023-10-08 21:50:22 +0200

    initial setup

---
+++ scripts/opdrachtOne/opdracht-1.js
@@ -0,0 +1,14 @@
+
+//Opdracht-1a
+console.log('De afdeling sales heeft ' + departments.sales.numberOfEmployees + ' medewerkers')
+
+//Opdracht-1b
+console.log('Marketing is een leuke afdeling om te werken. ' + departments.marketing.description + ' medewerkers')
+
+//Opdracht-1c
+console.log ('de afdeling cosutomer service heeft ' + departments["customer-service"].numberOfEmployees + ' medewerkers')
+
+//Opdracht-1d
+console.log('Sales is een uitdagende afdeling om te werken als Verkoopmanager. ' + departments.sales.jobs[1].description + ' ')
+
+
---
+++ scripts/opdrachtThree/opdracht-3.js
@@ -0,0 +1,28 @@
+/*
+//Opdracht: 3a
+const jobChoice = prompt (`Je koos marketing. Over welke functie wil je meer weten? Voer een getal tussen 0 en 4 in. 0: ${departments.marketing.jobs[0].title} 1: ${departments.marketing.jobs[1].title} 2: ${departments.marketing.jobs[2].title} 3: ${departments.marketing.jobs[3].title} 4: ${departments.marketing.jobs[4].title}`);
+
+console.log(jobChoice);
+
+//Opdracht: 3b & 3c
+
+switch (jobChoice){
+    case '0':
+        console.log(`Je koos ${departments.marketing.jobs[0].title}. Een uitdagende rol! ${departments.marketing.jobs[0].description}`)
+        break;
+    case '1':
+        console.log('Je koos ' + departments.marketing.jobs[1].title + ' Een uitdagende rol! ' + departments.marketing.jobs[1].description + ' ')
+    case '2':
+        console.log(`Je koos ${departments.marketing.jobs[2].title}. Een uitdagende rol! ${departments.marketing.jobs[2].description}`)
+        break;
+    case '3':
+        console.log(`Je koos ${departments.marketing.jobs[3].title}. Een uitdagende rol! ${departments.marketing.jobs[3].description}`)
+        break;
+    case '4':
+        console.log(`Je koos ${departments.marketing.jobs[4].title}. Een uitdagende rol! ${departments.marketing.jobs[4].description}`)
+        break;
+    default:
+        console.error('Ongeldige keuze')
+}
+
+*/
